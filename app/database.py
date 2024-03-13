20996c46511322dbce8c52cd58c528fe560adc9f
Gurveer1510 <singhgurveer4691@gmail.com>
2024-03-14 01:17:17 +0530

    .

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ from .config import settings
 
 url = URL.create('postgresql', username=settings.database_username, password=settings.database_password, host=settings.database_hostname,port=settings.database_port, database=settings.database_name)
 
-engine = create_engine(url='postgres://fastapi_6d8i_user:x9ndmCBKhMA1kEFwrohCvfM2qYaeeSUb@dpg-cnopajvjbltc73fk4kt0-a.oregon-postgres.render.com/fastapi_6d8i')
+engine = create_engine(url=url)
 
 SessionLocal = sessionmaker(autoflush= False, bind=engine)
 
