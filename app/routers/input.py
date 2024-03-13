115282ab6a5529e1c6e1a7d1a551422e6e7542dc
Gurveer1510 <singhgurveer4691@gmail.com>
2024-03-14 01:06:09 +0530

    final commit

--- app/routers/input.py
+++ app/routers/input.py
@@ -7,9 +7,15 @@ from ..database import get_db
 
 router = APIRouter(tags=['input'])
 
-@router.post("/input/", status_code=status.HTTP_201_CREATED)
+@router.post("/input", status_code=status.HTTP_201_CREATED)
 def get_url(long_url: str = Query(...), db: Session = Depends(get_db)):
-    # print(generate_random_key())
+    
+    url = db.query(models.Url).filter(models.Url.original_url == long_url).first()
+    print(url.original_url)
+    if url :
+        return{
+            "short_url": url.short_url
+        }
     short_key = generate_random_key()
     new_entry = models.Url(original_url=long_url, short_url=short_key)
     db.add(new_entry)
