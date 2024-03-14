542ce269fadbf3399a8e09f8a0ada63707d7f0cb
Gurveer1510 <singhgurveer4691@gmail.com>
2024-03-14 16:52:11 +0530

    .

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from .database import engine
 from .routers import input, redirect
@@ -8,13 +7,13 @@ from . import models
 app = FastAPI()
 
 origins = [
-    "*",
+    "*"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -24,11 +23,13 @@ models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+@app.get("/")
+def greet():
+    return {
+        "message" : "hello user"
+    }
+
 app.include_router(input.router)
 app.include_router(redirect.router)
-
-@app.get("/typer")
-async def redirect_typer():
-    return RedirectResponse("https://typer.tiangolo.com")
 
 
